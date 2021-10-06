3c8fb4d5b5e97e2a7c1501c460dbe55fa1ae0ed9
Doug Waldron <douglas.waldron@dnr.ga.gov>
2021-10-06 17:22:41 -0400

    Update GORA queries to include longer TLDs
    closes #487

---
+++ DB Objects/gora.ComplaintCounts.sql
@@ -0,0 +1,49 @@
+USE ComplaintTracking;
+GO
+SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+/*******************************************************************************
+
+Author:     Doug Waldron
+Created:    2017-12-21
+Overview:   Counts of complaints/actions in the database
+
+Modification History:
+When        Who                 What
+----------  ------------------  ------------------------------------------------
+2017-12-21  DWaldron            Initial Version
+
+*******************************************************************************/
+
+CREATE OR ALTER VIEW gora.ComplaintCounts
+AS
+
+SELECT 'Open Complaints' AS [Table],
+       count(*)          AS [Count]
+FROM Complaints c
+WHERE c.Deleted = 0       -- Not deleted
+  AND ComplaintClosed = 0 -- Not Closed
+
+UNION
+
+SELECT 'Closed Complaints',
+       count(*)
+FROM Complaints c
+WHERE c.Deleted = 0       -- Not deleted
+  AND ComplaintClosed = 1 -- Closed
+
+UNION
+
+SELECT 'Complaint Actions (closed complaints)',
+       count(*)
+FROM ComplaintActions ca
+    INNER JOIN Complaints c
+    ON ca.ComplaintId = c.Id
+WHERE ca.Deleted = 0 -- Not deleted
+  AND c.Deleted = 0  -- Not deleted
+  AND c.ComplaintClosed = 1; -- Closed
+
+GO
---
+++ DB Objects/gora.OpenComplaints.sql
@@ -0,0 +1,103 @@
+USE ComplaintTracking;
+GO
+SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+/*******************************************************************************
+
+Author:     Doug Waldron
+Created:    2017-12-21
+Overview:   Redacted view of closed CTS complaints
+
+Part of a set of queries used to respond to a public GORA request for 
+an updated copy of the CTS database 
+
+Modification History:
+When        Who                 What
+----------  ------------------  ------------------------------------------------
+2017-12-21  DWaldron            Initial Version
+2021-10-06  DWaldron            Update email regex
+
+---
+
+Notes:
+
+Uses SQL Server Regex Functions to redact email and phone number data.
+See https://github.com/gaepdit/SqlServerRegexFunctions
+
+Phone number RegEx: https://regexr.com/3h4h0
+Email RegEx: https://regexr.com/670vn
+Replacement email based on https://tools.ietf.org/html/rfc2606#section-2
+
+*******************************************************************************/
+
+CREATE OR ALTER VIEW gora.OpenComplaints
+AS
+
+SELECT c.Id                                    AS [ComplaintId],
+       c.CallerCity,
+       c.CallerName,
+       c.CallerPostalCode,
+       c.CallerRepresents,
+       -- CallerStateId,
+       callerState.Name                        AS CallerState,
+       c.CallerStreet,
+       c.CallerStreet2,
+       c.ComplaintCity,
+       -- ComplaintClosed,
+       -- ComplaintCountyId,
+       complaintCounty.Name                    AS ComplaintCounty,
+       c.ComplaintDirections,
+       -- ComplaintLocation,
+       dbo.RegexReplace(
+               dbo.RegexReplace(
+                       c.ComplaintLocation,
+                       N'\b\d{3}[- .]\d{4}\b',
+                       'xxx-xxxx'),
+               N'\b[\w\.-]+@[\w\.-]+\.\w{2,63}\b',
+               '[email@removed.invalid]')      AS ComplaintLocation,
+       -- ComplaintNature,
+       IIF(len(c.ComplaintNature) > 32600,
+           concat('[This entry has been truncated because it was longer than 32,600 characters] ',
+                  CHAR(13), CHAR(10),
+                  left(dbo.RegexReplace(
+                               dbo.RegexReplace(
+                                       c.ComplaintNature,
+                                       N'\b\d{3}[- .]\d{4}\b',
+                                       'xxx-xxxx'),
+                               N'\b[\w\.-]+@[\w\.-]+\.\w{2,63}\b',
+                               '[email@removed.invalid]'), 32600)),
+           dbo.RegexReplace(
+                   dbo.RegexReplace(
+                           c.ComplaintNature,
+                           N'\b\d{3}[- .]\d{4}\b',
+                           'xxx-xxxx'),
+                   N'\b[\w\.-]+@[\w\.-]+\.\w{2,63}\b',
+                   '[email@removed.invalid]')) AS ComplaintNature,
+       -- PrimaryConcernId,
+       primaryConcern.Name                     AS PrimaryConcern,
+       -- SecondaryConcernId,
+       secondaryConcern.Name                   AS SecondaryConcern,
+       c.DateReceived,
+       -- ReceivedById,
+       IIF(c.ReceivedById IS NULL, NULL,
+           concat_ws(', ', receivedBy.LastName, receivedBy.FirstName))
+                                               AS ReceivedBy
+       -- Status
+FROM Complaints c
+    LEFT JOIN LookupStates callerState
+    ON c.CallerStateId = callerState.Id
+    LEFT JOIN LookupCounties complaintCounty
+    ON c.ComplaintCountyId = complaintCounty.Id
+    LEFT JOIN LookupConcerns primaryConcern
+    ON c.PrimaryConcernId = primaryConcern.Id
+    LEFT JOIN AspNetUsers receivedBy
+    ON c.ReceivedById = receivedBy.Id
+    LEFT JOIN LookupConcerns secondaryConcern
+    ON c.SecondaryConcernId = secondaryConcern.Id
+WHERE c.Deleted = 0 -- Not deleted
+  AND c.ComplaintClosed = 0 -- Not Closed
+
+GO
