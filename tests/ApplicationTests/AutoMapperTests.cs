7f51739927c39c64447cb03dc6dd80b6f8656160
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-07-28 17:36:11 -0400

    Add AutoMapper validation tests

---
+++ tests/ApplicationTests/AutoMapperTests.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+using Cts.Application.ActionTypes;
+using Cts.Application.Offices;
+using Cts.Domain.ActionTypes;
+using FluentAssertions;
+
+namespace ApplicationTests;
+
+public class AutoMapperTests
+{
+    [Test]
+    public void ActionTypeMappingWorks()
+    {
+        var id = Guid.NewGuid();
+        const string name = "Name";
+        var actionType = new ActionType(id, name);
+
+        var result = Global.Mapper!.Map<ActionTypeViewDto>(actionType);
+
+        result.Id.Should().Be(id);
+        result.Name.Should().Be(name);
+        result.Active.Should().BeTrue();
+    }
+
+    [Test]
+    public void MappingConfigurationsAreValid()
+    {
+        var mapperConfig = new MapperConfiguration(c =>
+        {
+            c.AddProfile(new ActionTypeMappingProfile());
+            c.AddProfile(new OfficeMappingProfile());
+        });
+        mapperConfig.AssertConfigurationIsValid();
+    }
+}
