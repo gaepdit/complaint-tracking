09198ee3d90795312b194d7cfd488f2ec7b07015
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-09-15 12:42:20 -0400

    Use generated regex attributes

--- src/Domain/DataProcessing/PersonalInformation.cs
+++ src/Domain/DataProcessing/PersonalInformation.cs
@@ -1,25 +1,14 @@
-﻿using System.Text.RegularExpressions;
+﻿namespace Cts.Domain.DataProcessing;
 
-namespace Cts.Domain.DataProcessing;
-
-public static class PersonalInformation
+public static partial class PersonalInformation
 {
-    //-- Email RegEx: https://regex101.com/r/umVx9J/1
-    private const string SimpleEmailPattern = @"\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b";
-
-    //-- Replacement email based on https://tools.ietf.org/html/rfc2606#section-2
-    private const string EmailReplacementText = "[email@removed.invalid]";
-
-    //-- Phone number RegEx: https://regex101.com/r/xbOGha/1
-    private const string SimplePhonePattern = @"\b\d{3}[- .]\d{4}\b";
-    private const string PhoneReplacementText = "[phone number removed]";
-
     public static string? RedactPii(string? input)
     {
         if (string.IsNullOrWhiteSpace(input)) return input;
 
-        input = Regex.Replace(input, SimpleEmailPattern, EmailReplacementText);
-        input = Regex.Replace(input, SimplePhonePattern, PhoneReplacementText);
+        // Replacement email based on https://tools.ietf.org/html/rfc2606#section-2
+        input = Regexes.EmailRegex().Replace(input, "[email@removed.invalid]");
+        input = Regexes.PhoneRegex().Replace(input, "[phone number removed]");
 
         return input;
     }
---
+++ src/Domain/DataProcessing/Regexes.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Cts.Domain.DataProcessing;
+
+public static partial class Regexes
+{
+    //-- Email RegEx: https://regex101.com/r/umVx9J/1
+    private const string SimpleEmailPattern = @"\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b";
+
+    //-- Phone number RegEx: https://regex101.com/r/xbOGha/1
+    private const string SimplePhonePattern = @"\b\d{3}[- .]\d{4}\b";
+
+    [GeneratedRegex(SimpleEmailPattern)]
+    public static partial Regex EmailRegex();
+
+    [GeneratedRegex(SimplePhonePattern)]
+    public static partial Regex PhoneRegex();
+}
