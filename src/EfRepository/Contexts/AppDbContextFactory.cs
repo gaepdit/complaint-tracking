29bb7a39f294e0dd4ae9ed162c8f1f15bdc71551
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-11-17 12:09:23 -0500

    Remove DateOnlyTimeOnly NuGet package

--- src/EfRepository/Contexts/AppDbContextFactory.cs
+++ src/EfRepository/Contexts/AppDbContextFactory.cs
@@ -13,9 +13,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=cts-app;",
-            // FUTURE: This will no longer be necessary after upgrading to .NET 8.
-            builder => builder.UseDateOnlyTimeOnly());
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=cts-app;");
         return new AppDbContext(optionsBuilder.Options);
     }
 }
