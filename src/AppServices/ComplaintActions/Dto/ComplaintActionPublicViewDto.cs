7494b52416dd4684062591b82d2c904316be020d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-01-29 13:30:40 -0500

    Update public complaint view

--- src/AppServices/ComplaintActions/Dto/ComplaintActionPublicViewDto.cs
+++ src/AppServices/ComplaintActions/Dto/ComplaintActionPublicViewDto.cs
@@ -1,8 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Cts.AppServices.ComplaintActions;
 
 public record ComplaintActionPublicViewDto
 {
-    public DateOnly ActionDate { get; init; }
+    public Guid Id { get; [UsedImplicitly] init; }
+
+    [Display(Name = "Action Type")]
     public string ActionTypeName { get; init; } = default!;
+
+    [Display(Name = "Action Date")]
+    public DateOnly ActionDate { get; init; }
+
+    [Display(Name = "Investigator")]
+    public string? Investigator { get; init; }
+
     public string? Comments { get; init; }
 }
