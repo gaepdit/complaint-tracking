2434aa2b3d3e044cb5e2bc6a3aeba869a0c81a4e
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-02-16 16:30:53 -0500

    Implement Approve Complaint Page

--- src/AppServices/Complaints/Dto/Command/ComplaintClosureDto.cs
+++ src/AppServices/Complaints/Dto/Command/ComplaintClosureDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cts.AppServices.Complaints;
 
-// Used for closing, reopening, deleting, and restoring complaints.
+// Used for approving/closing, reopening, deleting, and restoring complaints.
 public record ComplaintClosureDto(int ComplaintId)
 {
     public string? Comment { get; init; } = string.Empty;
