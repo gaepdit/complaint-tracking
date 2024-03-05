97aa9b062d469e78020267bd62f2a3b147e736db
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-03-05 16:21:51 -0500

    Save enums as strings in the database
    Ref #604

--- src/EfRepository/Contexts/AppDbContext.cs
+++ src/EfRepository/Contexts/AppDbContext.cs
@@ -80,11 +80,10 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
 
         // === Additional configuration ===
 
-#pragma warning disable S125
         // Let's save enums in the database as strings.
-        // See https://stackoverflow.com/a/55260541/212978
-        // builder.Entity<EntityWithEnum>().Property(d => d.MyEnum).HasConversion(new EnumToStringConverter<MyEnumType>());
-#pragma warning restore S125
+        // See https://learn.microsoft.com/en-us/ef/core/modeling/value-conversions?tabs=data-annotations#pre-defined-conversions
+        builder.Entity<Complaint>().Property(complaint => complaint.Status).HasConversion<string>();
+        builder.Entity<ComplaintTransition>().Property(transition => transition.TransitionType).HasConversion<string>();
 
         // ## The following configurations are Sqlite only. ##
         if (Database.ProviderName != SqliteProvider) return;
@@ -93,7 +92,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbCo
         // Sqlite and EF Core are in conflict on how to handle collections of owned types.
         // See: https://stackoverflow.com/a/69826156/212978
         // and: https://learn.microsoft.com/en-us/ef/core/modeling/owned-entities#collections-of-owned-types
-        // builder.Entity<EntityWithOwnedType>().OwnsMany(e => e.MyOwnedTypeProperty, b => b.HasKey("Id"));
+        // builder.Entity<EntityWithOwnedTypeCollection>().OwnsMany(e => e.OwnedTypeCollection, b => b.HasKey("Id"));
 #pragma warning restore S125
 
         // "Handling DateTimeOffset in SQLite with Entity Framework Core"
