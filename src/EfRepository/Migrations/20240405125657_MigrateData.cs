a08a317e494025472fc0950112b7da944cfa4fa9
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-04-11 16:01:46 -0400

    Start EF data migration

---
+++ src/EfRepository/Migrations/20240405125657_MigrateData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cts.EfRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
