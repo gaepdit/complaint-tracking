f229c80e3cfc6913a0c15ddd6775de06da8adac7
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-04-15 16:11:11 -0400

    Migrate attachments

---
+++ src/EfRepository/Migrations/DataMigration/Migrate.Attachments.cs
@@ -0,0 +1,41 @@
+﻿namespace Cts.EfRepository.Migrations.DataMigration;
+
+public static partial class Migrate
+{
+    // language=sql
+    public const string Attachments =
+        """
+        insert into Attachments
+            (Id,
+             ComplaintId,
+             FileName,
+             FileExtension,
+             Size,
+             UploadedDate,
+             UploadedById,
+             IsImage,
+             CreatedAt,
+             CreatedById,
+             UpdatedAt,
+             UpdatedById,
+             IsDeleted,
+             DeletedAt,
+             DeletedById)
+        select lower(Id)                                         as Id,
+               ComplaintId,
+               trim(FileName)                                    as FileName,
+               FileExtension,
+               Size,
+               DateUploaded at time zone 'Eastern Standard Time' as DateUploaded,
+               lower(UploadedById)                               as UploadedById,
+               IsImage,
+               null                                              as CreatedAt,
+               null                                              as CreatedById,
+               null                                              as UpdatedAt,
+               null                                              as UpdatedById,
+               Deleted,
+               DateDeleted at time zone 'Eastern Standard Time'  as DateDeleted,
+               lower(DeletedById)                                as DeletedById
+        from _archive_Attachments;
+        """;
+}
