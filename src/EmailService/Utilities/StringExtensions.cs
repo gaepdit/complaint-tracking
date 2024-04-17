a6425b2cb1252a8809dbb2ce053f3d359c7e76bd
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-04-17 12:49:22 -0400

    Enable email message auditing

---
+++ src/EmailService/Utilities/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace GaEpd.EmailService.Utilities;
+
+public static class StringExtensions
+{
+    public static string ConcatWithSeparator(this IEnumerable<string?> items, string separator = " ") =>
+        string.Join(separator, items.Where(s => !string.IsNullOrEmpty(s)));
+}
