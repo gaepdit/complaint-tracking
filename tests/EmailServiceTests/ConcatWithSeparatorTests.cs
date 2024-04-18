fe0257d916fa3007499216289800b8bbeae3503d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-04-18 14:59:10 -0400

    Add additional unit tests

---
+++ tests/EmailServiceTests/ConcatWithSeparatorTests.cs
@@ -0,0 +1,31 @@
+﻿using GaEpd.EmailService.Utilities;
+
+namespace EmailServiceTests;
+
+[TestFixture]
+public class ConcatWithSeparatorTests
+{
+    [Test]
+    public void ConcatWithSeparator_WithDefaultSeparator()
+    {
+        new[] { "a", "b" }.ConcatWithSeparator().Should().Be("a b");
+    }
+
+    [Test]
+    public void ConcatWithSeparator_WithCustomSeparator()
+    {
+        new[] { "a", "b" }.ConcatWithSeparator("X").Should().Be("aXb");
+    }
+
+    [Test]
+    public void ConcatWithSeparator_WithNullValues()
+    {
+        new[] { "a", null, "b", null }.ConcatWithSeparator().Should().Be("a b");
+    }
+
+    [Test]
+    public void ConcatWithSeparator_WithEmptyStrings()
+    {
+        new[] { "a", "", "b", "" }.ConcatWithSeparator().Should().Be("a b");
+    }
+}
