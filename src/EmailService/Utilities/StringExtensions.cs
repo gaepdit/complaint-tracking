b45379b39d3dcbce0d5691c09dcf517e199db806
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-04-18 14:58:46 -0400

    Allow for multicharacter suffix when truncating string

--- src/EmailService/Utilities/StringExtensions.cs
+++ src/EmailService/Utilities/StringExtensions.cs
@@ -8,11 +8,14 @@ internal static class StringExtensions
         string.Join(separator, items.Where(s => !string.IsNullOrEmpty(s)));
 
     [return: NotNullIfNotNull(nameof(value))]
-    public static string? Truncate(this string? value, int maxLength, char suffix = '…')
+    public static string? Truncate(this string? value, int maxLength, string suffix = "…")
     {
         if (maxLength < 0) throw new ArgumentException("maxLength must not be negative.", nameof(maxLength));
         if (value is null) return null;
         if (string.IsNullOrEmpty(value) || maxLength == 0) return string.Empty;
-        return value.Length <= maxLength ? value : $"{value[..(maxLength - 1)]}{suffix}";
+        if (value.Length <= maxLength) return value;
+        return maxLength < suffix.Length
+            ? value[..maxLength]
+            : $"{value[..(maxLength - suffix.Length)]}{suffix}";
     }
 }
