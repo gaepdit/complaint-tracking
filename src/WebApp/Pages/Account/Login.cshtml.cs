2cd331b2e4fef8e7147668742ac020d52956e557
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-04-19 09:28:01 -0400

    Log out inactive user from login page

--- src/WebApp/Pages/Account/Login.cshtml.cs
+++ src/WebApp/Pages/Account/Login.cshtml.cs
@@ -1,14 +1,20 @@
-﻿namespace Cts.WebApp.Pages.Account;
+﻿using Cts.AppServices.Permissions;
+using Cts.AppServices.Permissions.Helpers;
+
+namespace Cts.WebApp.Pages.Account;
 
 [AllowAnonymous]
-public class LoginModel : PageModel
+public class LoginModel(IAuthorizationService authorization) : PageModel
 {
     public string? ReturnUrl { get; private set; }
 
-    public IActionResult OnGet(string? returnUrl = null)
+    public async Task<IActionResult> OnGetAsync(string? returnUrl = null)
     {
-        if (User.Identity?.IsAuthenticated ?? false)
+        if (User.Identity is { IsAuthenticated: true })
+        {
+            if (!await authorization.Succeeded(User, Policies.ActiveUser)) return RedirectToPage("Logout");
             return string.IsNullOrEmpty(returnUrl) ? RedirectToPage("/Index") : LocalRedirect(returnUrl);
+        }
 
         ReturnUrl = returnUrl;
         return Page();
