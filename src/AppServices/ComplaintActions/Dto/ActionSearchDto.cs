gaepdit/complaint-tracking

commit 6ba0f1f8341402c84a6f8f1a9952623ee861a048

Fix Null Reference when exporting complaints without a Received By set

src/Domain/Entities/Complaints/Complaint.cs
@@ -36,7 +36,7 @@ public class Complaint : AuditableSoftDeleteEntity<int>
 
     public DateTimeOffset ReceivedDate { get; set; }
 
-    public ApplicationUser ReceivedBy { get; set; } = default!;
+    public ApplicationUser? ReceivedBy { get; set; } = default!;
 
     // Properties: Caller
 

src/AppServices/DataExport/SearchResultsExportDto.cs
@@ -10,7 +10,7 @@ public record SearchResultsExportDto
     public SearchResultsExportDto(Complaint complaint)
     {
         ComplaintId = complaint.Id;
-        ReceivedByName = complaint.ReceivedBy.SortableFullName;
+        ReceivedByName = complaint.ReceivedBy?.SortableFullName;
         DateReceived = complaint.ReceivedDate;
         Status = complaint.Status.GetDisplayName();
         DateComplaintClosed = complaint.ComplaintClosedDate;
@@ -35,7 +35,7 @@ public record SearchResultsExportDto
     public int ComplaintId { get; init; }
 
     [XLColumn(Header = "Received By")]
-    public string ReceivedByName { get; init; }
+    public string? ReceivedByName { get; init; }
 
     [XLColumn(Header = "Date Received")]
     public DateTimeOffset DateReceived { get; init; }

src/EfRepository/Repositories/DataViewRepository.cs
@@ -10,6 +10,9 @@ namespace Cts.EfRepository.Repositories;
 
 public sealed class DataViewRepository(AppDbContext context, IDbConnectionFactory dbConnection) : IDataViewRepository
 {
+    // Only needed until Dapper supports DateOnly.
+    private readonly TimeOnly _midnight = new(hour: 0, minute: 0, second: 0);
+
     // Data archive export
     public Task<List<OpenComplaint>> OpenComplaintsAsync(CancellationToken token) =>
         context.OpenComplaintsView.ToListAsync(cancellationToken: token);
@@ -29,13 +32,12 @@ public sealed class DataViewRepository(AppDbContext context, IDbConnectionFactor
 
     public async Task<List<StaffReportView>> ComplaintsByStaffAsync(Guid officeId, DateOnly dateFrom, DateOnly dateTo)
     {
-        var midnight = new TimeOnly(0, 0, 0); // Only needed until Dapper supports DateOnly.
         return await QueryStaffReportAsync(ReportingQueries.ComplaintsByStaff,
             new
             {
                 officeId,
-                dateFrom = dateFrom.ToDateTime(midnight),
-                dateTo = dateTo.ToDateTime(midnight),
+                dateFrom = dateFrom.ToDateTime(_midnight),
+                dateTo = dateTo.ToDateTime(_midnight),
             }).ConfigureAwait(false);
     }
 
@@ -47,11 +49,10 @@ public sealed class DataViewRepository(AppDbContext context, IDbConnectionFactor
     public async Task<List<OfficeReportView>> DaysToClosureByOfficeAsync(DateOnly dateFrom, DateOnly dateTo,
         bool includeAdminClosed)
     {
-        var midnight = new TimeOnly(0, 0, 0); // Only needed until Dapper supports DateOnly.
         var parameters = new
         {
-            dateFrom = dateFrom.ToDateTime(midnight),
-            dateTo = dateTo.ToDateTime(midnight),
+            dateFrom = dateFrom.ToDateTime(_midnight),
+            dateTo = dateTo.ToDateTime(_midnight),
             includeAdminClosed,
         };
         using var db = dbConnection.Create();
@@ -63,13 +64,12 @@ public sealed class DataViewRepository(AppDbContext context, IDbConnectionFactor
     public async Task<List<StaffReportView>> DaysToClosureByStaffAsync(Guid officeId, DateOnly dateFrom,
         DateOnly dateTo, bool includeAdminClosed)
     {
-        var midnight = new TimeOnly(0, 0, 0); // Only needed until Dapper supports DateOnly.
         return await QueryStaffReportAsync(ReportingQueries.DaysToClosureByStaff,
             new
             {
                 officeId,
-                dateFrom = dateFrom.ToDateTime(midnight),
-                dateTo = dateTo.ToDateTime(midnight),
+                dateFrom = dateFrom.ToDateTime(_midnight),
+                dateTo = dateTo.ToDateTime(_midnight),
                 includeAdminClosed,
             }).ConfigureAwait(false);
     }
@@ -77,13 +77,12 @@ public sealed class DataViewRepository(AppDbContext context, IDbConnectionFactor
     public async Task<List<StaffReportView>> DaysToFollowupByStaffAsync(Guid officeId, DateOnly dateFrom,
         DateOnly dateTo)
     {
-        var midnight = new TimeOnly(0, 0, 0); // Only needed until Dapper supports DateOnly.
         return await QueryStaffReportAsync(ReportingQueries.DaysToFollowupByStaff,
             new
             {
                 officeId,
-                dateFrom = dateFrom.ToDateTime(midnight),
-                dateTo = dateTo.ToDateTime(midnight),
+                dateFrom = dateFrom.ToDateTime(_midnight),
+                dateTo = dateTo.ToDateTime(_midnight),
             }).ConfigureAwait(false);
     }
 

src/WebApp/Pages/Admin/Users/Index.cshtml
@@ -4,7 +4,7 @@
 @using Cts.WebApp.Platform.PageModelHelpers
 @model UsersIndexModel
 @{
-    ViewData["Title"] = "Application User Search";
+    ViewData["Title"] = "CTS User Search";
 }
 
 <h1>@ViewData["Title"]</h1>

commit a1ac77335a1473570dc8010d52c7a78f4de88121

Change default sort order for Complaint and Complaint Action search (#773)
* Update site map

* Change default sort order for search

* Fix usage of nullable Received By property - Issue introduced in 6ba0f1f8

src/AppServices/ComplaintActions/Dto/ActionSearchDto.cs
@@ -8,7 +8,7 @@ public record ActionSearchDto
 {
     // Sorting
 
-    public SortBy Sort { get; init; } = SortBy.IdAsc;
+    public SortBy Sort { get; init; } = SortBy.DateDesc;
 
     // Spec
 

src/AppServices/Complaints/ComplaintFilters.cs
@@ -117,7 +117,9 @@ internal static class ComplaintFilters
 
     private static Expression<Func<Complaint, bool>> ReceivedBy(this Expression<Func<Complaint, bool>> predicate,
         string? input) =>
-        string.IsNullOrWhiteSpace(input) ? predicate : predicate.And(complaint => complaint.ReceivedBy.Id == input);
+        string.IsNullOrWhiteSpace(input)
+            ? predicate
+            : predicate.And(complaint => complaint.ReceivedBy != null && complaint.ReceivedBy.Id == input);
 
     private static Expression<Func<Complaint, bool>> ContainsCaller(this Expression<Func<Complaint, bool>> predicate,
         string? input) =>
@@ -180,7 +182,7 @@ internal static class ComplaintFilters
             : predicate.And(complaint =>
                 complaint.SourceAddress != null &&
                 ((complaint.SourceAddress.Street != null && complaint.SourceAddress.Street.Contains(input)) ||
-                    (complaint.SourceAddress.Street2 != null && complaint.SourceAddress.Street2.Contains(input))));
+                 (complaint.SourceAddress.Street2 != null && complaint.SourceAddress.Street2.Contains(input))));
 
     private static Expression<Func<Complaint, bool>> ContainsCity(this Expression<Func<Complaint, bool>> predicate,
         string? input) =>

src/AppServices/Complaints/QueryDto/ComplaintSearchDto.cs
@@ -6,7 +6,7 @@ public record ComplaintSearchDto
 {
     // Sorting
 
-    public SortBy Sort { get; init; } = SortBy.IdAsc;
+    public SortBy Sort { get; init; } = SortBy.IdDesc;
 
     // Status
 

src/Domain/Entities/Complaints/Complaint.cs
@@ -36,7 +36,7 @@ public class Complaint : AuditableSoftDeleteEntity<int>
 
     public DateTimeOffset ReceivedDate { get; set; }
 
-    public ApplicationUser? ReceivedBy { get; set; } = default!;
+    public ApplicationUser? ReceivedBy { get; set; }
 
     // Properties: Caller
 

tests/EfRepositoryTests/Complaints/PublicSearchSpec.cs
@@ -38,7 +38,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
             .Excluding(e => e.ReviewedBy!.Office)
         );
@@ -65,7 +65,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }
@@ -87,7 +87,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }
@@ -109,7 +109,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }
@@ -131,7 +131,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }
@@ -153,7 +153,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }
@@ -176,7 +176,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }
@@ -200,7 +200,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }
@@ -223,7 +223,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }
@@ -246,7 +246,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }
@@ -270,7 +270,7 @@ public class PublicSearchSpec
             .Excluding(e => e.Actions)
             .Excluding(e => e.ComplaintTransitions)
             .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy.Office)
+            .Excluding(e => e.ReceivedBy!.Office)
             .Excluding(e => e.CurrentOwner!.Office)
         );
     }