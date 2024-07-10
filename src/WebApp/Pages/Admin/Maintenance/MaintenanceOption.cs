3a5fa68399e689cf1c7e7da5b1574c4ffcb9fbaa
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2024-07-10 09:21:52 -0400

    Show a warning message when editing a maintenance item (#796)

--- src/WebApp/Pages/Admin/Maintenance/MaintenanceOption.cs
+++ src/WebApp/Pages/Admin/Maintenance/MaintenanceOption.cs
@@ -2,17 +2,24 @@ namespace Cts.WebApp.Pages.Admin.Maintenance;
 
 public class MaintenanceOption
 {
-    public string SingularName { get; private init; } = string.Empty;
-    public string PluralName { get; private init; } = string.Empty;
+    public string SingularName { get; private init; }
+    public string PluralName { get; private init; }
+    public bool StartsWithVowelSound { get; private init; }
 
-    private MaintenanceOption() { }
+    private MaintenanceOption(string singularName, string pluralName, bool startsWithVowelSound = false)
+    {
+        SingularName = singularName;
+        PluralName = pluralName;
+        StartsWithVowelSound = startsWithVowelSound;
+    }
 
     public static MaintenanceOption ActionType { get; } =
-        new() { SingularName = "Complaint Action Type", PluralName = "Complaint Action Types" };
+        new(singularName: "Complaint Action Type", pluralName: "Complaint Action Types");
 
     public static MaintenanceOption Concern { get; } =
-        new() { SingularName = "Environmental Area of Concern", PluralName = "Environmental Areas of Concern" };
+        new(singularName: "Environmental Area of Concern", pluralName: "Environmental Areas of Concern",
+            startsWithVowelSound: true);
 
     public static MaintenanceOption Office { get; } =
-        new() { SingularName = "Office", PluralName = "Offices" };
+        new(singularName: "Office", pluralName: "Offices", startsWithVowelSound: true);
 }
