48c5208f9d512665d17bfa203c898630336f8819
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-07-25 08:47:54 -0400

    Show correct version and build on Support page and error logging

--- src/WebApp/Platform/AppConfiguration/BindingsConfiguration.cs
+++ src/WebApp/Platform/AppConfiguration/BindingsConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Cts.WebApp.Platform.Settings;
+using System.Reflection;
 
 namespace Cts.WebApp.Platform.AppConfiguration;
 
@@ -13,10 +14,18 @@ public static class BindingsConfiguration
         builder.Configuration.GetSection(nameof(AppSettings.RaygunSettings))
             .Bind(AppSettings.RaygunSettings);
 
+        // App versioning
+        var versionSegments = (Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+            .InformationalVersion ?? Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "").Split('+');
+
+        AppSettings.SupportSettings.InformationalVersion = versionSegments[0];
+        if (versionSegments.Length > 0)
+            AppSettings.SupportSettings.InformationalBuild = versionSegments[1][..Math.Min(7, versionSegments[1].Length)];
+
         // Dev settings
         var devConfig = builder.Configuration.GetSection(nameof(AppSettings.DevSettings));
         var useDevConfig = devConfig.Exists() &&
-            Convert.ToBoolean(devConfig[nameof(AppSettings.DevSettings.UseDevSettings)]);
+                           Convert.ToBoolean(devConfig[nameof(AppSettings.DevSettings.UseDevSettings)]);
 
         if (useDevConfig)
             devConfig.Bind(AppSettings.DevSettings);
