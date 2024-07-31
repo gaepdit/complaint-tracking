13e4fda8531114dadd3c7f99f8c318f35f6692f5
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-07-31 16:36:36 -0400

    Add Table of Contents to complaint details pages
    ---------
    
    Co-authored-by: Caleb Nguyen <caleb.nguyen@dnr.ga.gov>
    Co-authored-by: Doug Waldron <douglas.waldron@dnr.ga.gov>

---
+++ src/WebApp/wwwroot/js/table-of-contents.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const toc = document.getElementById('toc');
+    const headers = document.querySelectorAll('h2');
+
+    headers.forEach((header, index) => {
+        //if the h2 does not have an ID, assign one so we can use a tag
+        if (!header.id) {
+            header.id = `section${index + 1}`;
+        }
+
+        const link = document.createElement('a');
+        link.href = `#${header.id}`;
+        link.textContent = header.textContent;
+        link.classList.add('list-group-item', 'list-group-item-action');
+        toc.appendChild(link);
+    });
+});
