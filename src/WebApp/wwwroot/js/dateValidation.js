8cffcf971ce3487ebedcaa3985075fcc9634c947
Caleb Nguyen <caleb.nguyen@dnr.ga.gov>
2024-08-13 13:50:46 -0400

    Added formSearch.js contents to dateValidation.js

--- src/WebApp/wwwroot/js/dateValidation.js
+++ src/WebApp/wwwroot/js/dateValidation.js
@@ -20,5 +20,18 @@ document.getElementById("SearchButton").addEventListener("click", function (e) {
     );
     e.preventDefault();
   } else {
+    // (Add this script to search forms that use GET for submit. It keeps clutter out of the resulting query string.)
+    $(document).ready(function () {
+      function disableEmptyInput(n, el) {
+        const $input = $(el);
+        if ($input.val() === "") $input.attr("disabled", "disabled");
+      }
+
+      $("#SearchButton").click(function DisableEmptyInputs() {
+        $("input").each(disableEmptyInput);
+        $("select").each(disableEmptyInput);
+        return true;
+      });
+    });
   }
 });
