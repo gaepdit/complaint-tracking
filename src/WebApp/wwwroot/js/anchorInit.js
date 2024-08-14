9af6f01d87fdc78395cb2e9c5fb6035a37d4714b
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-08-14 14:15:44 -0400

    Fis potential error in Anchor JS init

--- src/WebApp/wwwroot/js/anchorInit.js
+++ src/WebApp/wwwroot/js/anchorInit.js
@@ -1,9 +1,7 @@
-﻿// Write your JavaScript code.
-
-const anchors = new AnchorJS();
-// DOMContentLoaded was tested to be the best place to call anchors.add()
-document.addEventListener('DOMContentLoaded', function () {
-    anchors.options.placement = "left";
-    // Add anchors to h2, h3, h4, h5, and h6 elements, but exclude any with the "no-anchor" class.
-    anchors.add('h2:not(.no-anchor), h3:not(.no-anchor)');
-})
+﻿if (typeof AnchorJS === 'function') {
+    const anchors = new AnchorJS();
+    document.addEventListener('DOMContentLoaded', function () {
+        anchors.options.placement = "left";
+        anchors.add('h2:not(.no-anchor), h3:not(.no-anchor)');
+    })
+}
