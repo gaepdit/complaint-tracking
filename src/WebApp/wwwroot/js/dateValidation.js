8fccb2f389a6bbdb08563ef3ca709809338d22ef
Caleb Nguyen <caleb.nguyen@dnr.ga.gov>
2024-08-16 12:48:28 -0400

    Added custom text for From/To dates of Recieved, Closed, and Entered

--- src/WebApp/wwwroot/js/dateValidation.js
+++ src/WebApp/wwwroot/js/dateValidation.js
@@ -4,28 +4,28 @@ document.getElementById("SearchButton").addEventListener("click", function (e) {
     {
       fromID: "Spec_DateFrom",
       toID: "Spec_DateTo",
-      from: "RECEIVED From Date",
-      to: "RECEIVED To Date",
+      from: "Received FROM Date ",
+      to: "Received THROUGH Date ",
     },
     //Staff/Complaints/Index.cshtml
     {
       fromID: "Spec_ReceivedFrom",
       toID: "Spec_ReceiveTo",
-      from: "RECIEVED From Date",
-      to: "RECIEVED To Date",
+      from: "Received FROM Date ",
+      to: "Received THROUGH Date ",
     },
     {
       fromID: "Spec_ClosedFrom",
       toID: "Spec_ClosedTo",
-      from: "CLOSED From Date",
-      to: "CLOSED To Date",
+      from: "Closed FROM Date ",
+      to: "Closed THROUGH Date ",
     },
     //Staff/ComplaintActions/Index.cshtml
     {
       fromID: "Spec_EnteredFrom",
       toID: "Spec_EnteredTo",
-      from: "ENTERED From Date",
-      to: "ENTERED To Date",
+      from: "Entered FROM Date ",
+      to: "Entered THROUGH Date ",
     },
   ];
 
@@ -35,38 +35,36 @@ document.getElementById("SearchButton").addEventListener("click", function (e) {
     var dateFrom = document.getElementById(pair.fromID).value;
     var dateTo = document.getElementById(pair.toID).value;
 
-    if (dateFrom != null || dateTo != null) {
-      if (dateTo > today) {
-        alert(
-          "Through date (" +
-            dateTo +
-            ") cannot exceed current date (" +
-            today +
-            ")"
-        );
-        e.preventDefault();
-        break;
-      } else if (dateFrom > today) {
-        alert(
-          "From date (" +
-            dateFrom +
-            ") cannot exceed current date (" +
-            today +
-            ")"
-        );
-        e.preventDefault();
-        break;
-      } else if (dateFrom > dateTo) {
-        alert(
-          "From date (" +
-            dateFrom +
-            ") cannot exceed through date (" +
-            dateTo +
-            ")"
-        );
-        e.preventDefault();
-        break;
-      }
+    if (dateTo && dateTo > today) {
+      alert(
+        pair.to + "(" + dateTo + ") cannot exceed current date (" + today + ")"
+      );
+      e.preventDefault();
+      break;
+    } else if (dateFrom && dateFrom > today) {
+      alert(
+        pair.from +
+          "(" +
+          dateFrom +
+          ") cannot exceed current date (" +
+          today +
+          ")"
+      );
+      e.preventDefault();
+      break;
+    } else if (dateTo && dateFrom && dateFrom > dateTo) {
+      alert(
+        pair.from +
+          "(" +
+          dateFrom +
+          ") cannot exceed " +
+          pair.to +
+          "(" +
+          dateTo +
+          ")"
+      );
+      e.preventDefault();
+      break;
     }
   }
   if (!e.defaultPrevented) {
