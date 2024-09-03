84cdf4247554ce1c5acb5620a0bae25b521e14da
Caleb Nguyen <caleb.nguyen@dnr.ga.gov>
2024-09-03 10:25:10 -0400

    date validation works, thank you doug!

--- src/WebApp/wwwroot/js/dateValidation.js
+++ src/WebApp/wwwroot/js/dateValidation.js
@@ -15,6 +15,7 @@ function datePairs(fromID, toID, fromString, toString) {
             today +
             ")"
         );
+        e.stopImmediatePropagation();
         e.preventDefault();
       } else if (dateFrom && dateFrom > today) {
         alert(
@@ -25,6 +26,7 @@ function datePairs(fromID, toID, fromString, toString) {
             today +
             ")"
         );
+        e.stopImmediatePropagation();
         e.preventDefault();
       } else if (dateTo && dateFrom && dateFrom > dateTo) {
         alert(
@@ -37,53 +39,8 @@ function datePairs(fromID, toID, fromString, toString) {
             dateTo +
             ")"
         );
+        e.stopImmediatePropagation();
         e.preventDefault();
-      }
-
-      if (!e.defaultPrevented) {
-        //formSearch.js
-        $(document).ready(function () {
-          function disableEmptyInput(n, el) {
-            const $input = $(el);
-            if ($input.val() === "") $input.attr("disabled", "disabled");
-          }
-
-          $("#SearchButton").click(function DisableEmptyInputs() {
-            $("input").each(disableEmptyInput);
-            $("select").each(disableEmptyInput);
-            return true;
-          });
-        });
       }
     });
 }
-
-/*const datePairs = [
-        //Index.cshtml
-        {
-          fromID: "Spec_DateFrom",
-          toID: "Spec_DateTo",
-          from: "Received FROM Date ",
-          to: "Received THROUGH Date ",
-        },
-        //Staff/Complaints/Index.cshtml
-        {
-          fromID: "Spec_ReceivedFrom",
-          toID: "Spec_ReceiveTo",
-          from: "Received FROM Date ",
-          to: "Received THROUGH Date ",
-        },
-        {
-          fromID: "Spec_ClosedFrom",
-          toID: "Spec_ClosedTo",
-          from: "Closed FROM Date ",
-          to: "Closed THROUGH Date ",
-        },
-        //Staff/ComplaintActions/Index.cshtml
-        {
-          fromID: "Spec_EnteredFrom",
-          toID: "Spec_EnteredTo",
-          from: "Entered FROM Date ",
-          to: "Entered THROUGH Date ",
-        },
-      ];*/
