1bc3c9ad084895a4b5cb5635f19185659ed74af1
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-11-05 09:41:25 -0500

    Reformat some JS files

--- src/WebApp/wwwroot/js/dateValidation.js
+++ src/WebApp/wwwroot/js/dateValidation.js
@@ -1,46 +1,23 @@
 function datePairs(fromID, toID, fromString, toString) {
-  document
-    .getElementById("SearchButton")
-    .addEventListener("click", function (e) {
-      const today = new Date().toISOString().slice(0, 10);
-      const dateFrom = document.getElementById(fromID).value;
-      const dateTo = document.getElementById(toID).value;
+    document
+        .getElementById("SearchButton")
+        .addEventListener("click", function (e) {
+            const today = new Date().toISOString().slice(0, 10);
+            const dateFrom = document.getElementById(fromID).value;
+            const dateTo = document.getElementById(toID).value;
 
-      if (dateTo && dateTo > today) {
-        alert(
-          toString +
-            " (" +
-            dateTo +
-            ") cannot exceed current date (" +
-            today +
-            ")"
-        );
-        e.stopImmediatePropagation();
-        e.preventDefault();
-      } else if (dateFrom && dateFrom > today) {
-        alert(
-          fromString +
-            " (" +
-            dateFrom +
-            ") cannot exceed current date (" +
-            today +
-            ")"
-        );
-        e.stopImmediatePropagation();
-        e.preventDefault();
-      } else if (dateTo && dateFrom && dateFrom > dateTo) {
-        alert(
-          fromString +
-            " (" +
-            dateFrom +
-            ") cannot exceed " +
-            toString +
-            " (" +
-            dateTo +
-            ")"
-        );
-        e.stopImmediatePropagation();
-        e.preventDefault();
-      }
-    });
+            if (dateTo && dateTo > today) {
+                alert(`${toString} (${dateTo}) cannot exceed current date (${today}).`);
+                e.stopImmediatePropagation();
+                e.preventDefault();
+            } else if (dateFrom && dateFrom > today) {
+                alert(`${fromString} (${dateFrom}) cannot exceed current date (${today}).`);
+                e.stopImmediatePropagation();
+                e.preventDefault();
+            } else if (dateTo && dateFrom && dateFrom > dateTo) {
+                alert(`${fromString} (${dateFrom}) cannot exceed ${toString} (${dateTo}).`);
+                e.stopImmediatePropagation();
+                e.preventDefault();
+            }
+        });
 }
--- src/WebApp/wwwroot/js/formSearch.js
+++ src/WebApp/wwwroot/js/formSearch.js
@@ -1,13 +1,13 @@
 // (Add this script to search forms that use GET for submit. It keeps clutter out of the resulting query string.)
 $(document).ready(function () {
-  function disableEmptyInput(n, el) {
-    const $input = $(el);
-    if ($input.val() === "") $input.attr("disabled", "disabled");
-  }
+    function disableEmptyInput(n, el) {
+        const $input = $(el);
+        if ($input.val() === "") $input.attr("disabled", "disabled");
+    }
 
-  $("#SearchButton").click(function DisableEmptyInputs() {
-    $("input").each(disableEmptyInput);
-    $("select").each(disableEmptyInput);
-    return true;
-  });
+    $("#SearchButton").click(function DisableEmptyInputs() {
+        $("input").each(disableEmptyInput);
+        $("select").each(disableEmptyInput);
+        return true;
+    });
 });
