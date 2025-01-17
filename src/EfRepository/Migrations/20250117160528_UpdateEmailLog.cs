ff97c91663bc04705f01ca5d22f8daaffa85fe96
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-01-17 11:06:20 -0500

    Add migration for email log repository

---
+++ src/EfRepository/Migrations/20250117160528_UpdateEmailLog.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cts.EfRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEmailLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sender",
+                table: "EmailLogs",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sender",
+                table: "EmailLogs",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+        }
+    }
+}
