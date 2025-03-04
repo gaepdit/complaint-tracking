e3716639e473e976bda9643172544089822eb3f8
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-03-04 17:01:02 -0500

    Display city and county of complaint in search results and export

--- src/AppServices/Complaints/QueryDto/ComplaintSearchResultDto.cs
+++ src/AppServices/Complaints/QueryDto/ComplaintSearchResultDto.cs
@@ -11,15 +11,14 @@ public record ComplaintSearchResultDto
     public bool ComplaintClosed { get; init; }
     public DateTimeOffset? ComplaintClosedDate { get; init; }
     public string? SourceFacilityName { get; init; }
-    public string? ComplaintLocation { get; init; }
     public string? SourceFacilityIdNumber { get; init; }
-    public string? SourceAddressCityState { get; init; }
+    public string? ComplaintCity { get; init; }
     public string? ComplaintCounty { get; init; }
+    public string? SourceAddressCityState { get; init; }
     public string? PrimaryConcernName { get; init; }
     public string? SecondaryConcernName { get; init; }
     public string? CurrentOfficeName { get; init; }
     public StaffViewDto? CurrentOwner { get; init; }
     public string? AssignedOwnerName => CurrentOwner?.SortableFullName;
     public bool IsDeleted { get; init; }
-
 }
