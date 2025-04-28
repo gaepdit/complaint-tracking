7a23892cce9c96e0c90ef63cd0c67290edb999e5
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-04-28 16:30:35 -0400

    Improve Short ID generator

--- src/WebApp/Platform/Logging/ShortId.cs
+++ src/WebApp/Platform/Logging/ShortId.cs
@@ -1,17 +1,9 @@
 ﻿using System.Text;
 
-namespace Cts.WebApp.Platform.ErrorLogging;
+namespace Cts.WebApp.Platform.Logging;
 
 public static class ShortId
 {
-    private static readonly Random Random = new();
     private const string Pool = "ABCDEFGHKMNPQRSTUVWXYZ2345689";
-
-    public static string GetShortId(int length = 5)
-    {
-        var poolLength = Pool.Length;
-        var output = new StringBuilder();
-        for (var i = 0; i < length; i++) output.Append(Pool[Random.Next(0, poolLength)]);
-        return output.ToString();
-    }
+    public static string GetShortId(int length = 5) => new(Random.Shared.GetItems<char>(Pool, length));
 }
