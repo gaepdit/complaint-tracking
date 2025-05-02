571047d2b7c6d86dbe1a8938f4e5bf67ad6ba07f
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-05-02 10:22:39 -0400

    Remove obsolete data migration scripts
    The data migration scripts were only needed for the deployment of CTS Model 4 on 2024-07-24. Their continued presence made it difficult to test Entity Framework migrations in future development.

--- src/EfRepository/Migrations/20240418193041_AddDatabaseViews.cs
+++ src/EfRepository/Migrations/20240418193041_AddDatabaseViews.cs
@@ -1,5 +1,4 @@
-﻿using Cts.EfRepository.Migrations.DatabaseView;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -11,11 +10,20 @@ namespace Cts.EfRepository.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // Add database views
-            migrationBuilder.Sql(View.OpenComplaintsView);
-            migrationBuilder.Sql(View.ClosedComplaintsView);
-            migrationBuilder.Sql(View.ClosedComplaintActionsView);
-            migrationBuilder.Sql(View.RecordsCountView);
+            // Code to add database views has been removed. These views rely on the RegexReplace function from the
+            // SqlServerRegexFunctions repo. Since that function is not available when creating a new database, the
+            // migration would cause errors.
+            // 
+            // The views that were added here are only used by the CTS data archive tool. To enable data archiving,
+            // add the regex functions and the following views to the database.
+            // 
+            // * SqlServerRegexFunctions: https://github.com/gaepdit/SqlServerRegexFunctions
+            // 
+            // * Database views (in the `DatabaseViews` folder):
+            //   - View.OpenComplaintsView.sql
+            //   - View.ClosedComplaintsView.sql
+            //   - View.ClosedComplaintActionsView.sql
+            //   - View.RecordsCountView.sql
         }
     }
 }
---
+++ src/EfRepository/Migrations/DatabaseViews/View.ClosedComplaintActionsView.sql
@@ -0,0 +1,52 @@
+﻿CREATE OR ALTER VIEW dbo.ClosedComplaintActionsView
+AS
+
+/**************************************************************************************************
+
+Author:     Doug Waldron
+Overview:
+  Redacted view of complaint actions for closed CTS complaints.
+  Part of a set of queries used to respond to public requests for an archived copy of the database.
+
+Modification History:
+When        Who                 What
+----------  ------------------  -------------------------------------------------------------------
+2017-12-21  DWaldron            Initial Version
+2021-10-06  DWaldron            Update email regex
+2024-02-29  DWaldron            Application rewrite
+
+---
+
+Notes:
+
+  Uses SQL Server Regex Functions to redact email and phone number data.
+  See https://github.com/gaepdit/SqlServerRegexFunctions
+  
+  Phone number RegEx: https://regexr.com/3h4h0
+  Email RegEx: https://regexr.com/670vn
+  Replacement email based on https://tools.ietf.org/html/rfc2606#section-2
+
+***************************************************************************************************/
+
+select a.ComplaintId   as [ComplaintId],
+       a.ActionDate    as [ActionDate],
+       actionType.Name as [ActionType],
+       dbo.RegexReplace(dbo.RegexReplace(a.Comments, N'\b\d{3}[- .]\d{4}\b', '[phone number removed]'),
+                        N'\b[\w\.-]+@[\w\.-]+\.\w{2,63}\b', '[email@removed.invalid]')
+                       as [Comments],
+       a.EnteredDate   as [EnteredDate],
+       IIF(a.EnteredById is null, null, concat_ws(', ', enteredBy.FamilyName, enteredBy.GivenName))
+                       as [EnteredBy],
+       a.Investigator  as [Investigator]
+from ComplaintActions a
+         inner join Complaints c
+                    on a.ComplaintId = c.Id
+         left join ActionTypes actionType
+                   on a.ActionTypeId = actionType.Id
+         left join AspNetUsers enteredBy
+                   on a.EnteredById = enteredBy.Id
+where a.IsDeleted = convert(bit, 0) -- Action not deleted
+  and c.IsDeleted = convert(bit, 0) -- Complaint not deleted
+  and c.ComplaintClosed = convert(bit, 1); -- Complaint closed
+
+GO
