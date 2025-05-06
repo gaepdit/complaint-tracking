3e4766d562962f06511ae6add5f988fdec8218ed
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-05-06 09:33:36 -0400

    Add EF migration

---
+++ src/EfRepository/Migrations/20250506133119_UpdateExternalAuthIds.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cts.EfRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateExternalAuthIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ObjectIdentifier",
+                table: "AspNetUsers",
+                newName: "EntraIdSubjectId");
+
+            migrationBuilder.RenameIndex(
+                name: "missing_index_198_197",
+                table: "AspNetUsers",
+                newName: "missing_index_198_197_A");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OktaSubjectId",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "missing_index_198_197_B",
+                table: "AspNetUsers",
+                column: "OktaSubjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "missing_index_198_197_B",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "OktaSubjectId",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "EntraIdSubjectId",
+                table: "AspNetUsers",
+                newName: "ObjectIdentifier");
+
+            migrationBuilder.RenameIndex(
+                name: "missing_index_198_197_A",
+                table: "AspNetUsers",
+                newName: "missing_index_198_197");
+        }
+    }
+}
