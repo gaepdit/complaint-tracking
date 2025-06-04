0e1bd91b998d811f4628de93b45c43c17450f99b
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-06-04 14:14:25 -0400

    Update NuGet packages

--- tests/AppServicesTests/AppServicesTestsSetup.cs
+++ tests/AppServicesTests/AppServicesTestsSetup.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
+using AwesomeAssertions.Extensions;
 using Cts.AppServices.AutoMapper;
-using FluentAssertions.Extensions;
 
 namespace AppServicesTests;
 
@@ -22,8 +22,8 @@ public class AppServicesTestsSetup
             .ExcludingMissingMembers()
 
             // DateTimeOffset comparison is often off by a few microseconds.
-            .Using<DateTimeOffset>(
-                context => context.Subject.Should().BeCloseTo(context.Expectation, 10.Milliseconds()))
+            .Using<DateTimeOffset>(context =>
+                context.Subject.Should().BeCloseTo(context.Expectation, 10.Milliseconds()))
             .WhenTypeIs<DateTimeOffset>()
         );
     }
--- tests/LocalRepositoryTests/GlobalUsings.cs
+++ tests/LocalRepositoryTests/GlobalUsings.cs
@@ -1,5 +1,5 @@
-global using FluentAssertions;
-global using FluentAssertions.Execution;
+global using AwesomeAssertions;
+global using AwesomeAssertions.Execution;
 global using NSubstitute;
 global using NUnit.Framework;
 
