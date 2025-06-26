gaepdit/complaint-tracking

commit 07bcbfbf87996d317a500ec559e656cd50bd12ea

Simplify unit test repository helpers (#912)

src/EfRepository/Contexts/SeedDevData/DbSeedDataHelpers.cs
@@ -7,11 +7,24 @@ public static class DbSeedDataHelpers
 {
     public static void SeedAllData(AppDbContext context)
     {
+        ClearAllStaticData();
         SeedActionTypeData(context);
         SeedConcernData(context);
         SeedOfficeData(context);
         SeedIdentityData(context);
         SeedComplaintData(context);
+    }
+
+    private static void ClearAllStaticData()
+    {
+        ActionTypeData.ClearData();
+        AttachmentData.ClearData();
+        ComplaintActionData.ClearData();
+        ComplaintData.ClearData();
+        ComplaintTransitionData.ClearData();
+        ConcernData.ClearData();
+        OfficeData.ClearData();
+        UserData.ClearData();
     }
 
     private static void SeedActionTypeData(AppDbContext context)
@@ -25,7 +38,6 @@ public static class DbSeedDataHelpers
     {
         if (context.Complaints.Any()) return;
 
-        context.Database.BeginTransaction();
         if (context.Database.ProviderName == AppDbContext.SqlServerProvider)
             context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Complaints ON");
 
@@ -34,7 +46,6 @@ public static class DbSeedDataHelpers
 
         if (context.Database.ProviderName == AppDbContext.SqlServerProvider)
             context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Complaints OFF");
-        context.Database.CommitTransaction();
 
         if (!context.Attachments.Any())
             context.Attachments.AddRange(AttachmentData.GetAttachments);

tests/EfRepositoryTests/RepositoryHelper.cs
@@ -3,8 +3,6 @@ using Cts.Domain.Entities.Offices;
 using Cts.EfRepository.Contexts;
 using Cts.EfRepository.Contexts.SeedDevData;
 using Cts.EfRepository.Repositories;
-using Cts.TestData;
-using Cts.TestData.Identity;
 using GaEpd.AppLibrary.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
@@ -37,9 +35,12 @@ public sealed class RepositoryHelper : IDisposable, IAsyncDisposable
     private RepositoryHelper()
     {
         _options = SqliteInMemory.CreateOptions<AppDbContext>(builder =>
-            builder.LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]));
+        {
+            builder.LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]);
+            builder.UseSeeding((context, _) => DbSeedDataHelpers.SeedAllData(context));
+        });
         _context = new AppDbContext(_options);
-        _context.Database.EnsureCreated();
+        _context.Database.EnsureClean();
     }
 
     /// <summary>
@@ -50,8 +51,11 @@ public sealed class RepositoryHelper : IDisposable, IAsyncDisposable
     private RepositoryHelper(object callingClass, string callingMember)
     {
         _options = callingClass.CreateUniqueMethodOptions<AppDbContext>(callingMember: callingMember,
-            builder: builder => builder.UseSqlServer()
-                .LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]));
+            builder: builder =>
+            {
+                builder.UseSqlServer().LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]);
+                builder.UseSeeding((context, _) => DbSeedDataHelpers.SeedAllData(context));
+            });
         _context = new AppDbContext(_options);
         _context.Database.EnsureClean();
     }
@@ -119,26 +123,12 @@ public sealed class RepositoryHelper : IDisposable, IAsyncDisposable
         ClearChangeTracker();
     }
 
-    private static void ClearAllStaticData()
-    {
-        ActionTypeData.ClearData();
-        AttachmentData.ClearData();
-        ComplaintActionData.ClearData();
-        ComplaintData.ClearData();
-        ComplaintTransitionData.ClearData();
-        ConcernData.ClearData();
-        OfficeData.ClearData();
-        UserData.ClearData();
-    }
-
     /// <summary>
     /// Seeds data and returns an instance of ActionTypeRepository.
     /// </summary>
     /// <returns>An <see cref="ComplaintRepository"/>.</returns>
     public IComplaintRepository GetComplaintRepository()
     {
-        ClearAllStaticData();
-        DbSeedDataHelpers.SeedAllData(_context);
         Context = new AppDbContext(_options);
         return new ComplaintRepository(Context);
     }
@@ -149,9 +139,6 @@ public sealed class RepositoryHelper : IDisposable, IAsyncDisposable
     /// <returns>An <see cref="OfficeRepository"/>.</returns>
     public IOfficeRepository GetOfficeRepository()
     {
-        ClearAllStaticData();
-        DbSeedDataHelpers.SeedOfficeData(_context);
-        DbSeedDataHelpers.SeedIdentityData(_context);
         Context = new AppDbContext(_options);
         return new OfficeRepository(Context);
     }

tests/LocalRepositoryTests/RepositoryHelper.cs
@@ -1,34 +1,10 @@
 ﻿using Cts.LocalRepository.Identity;
 using Cts.LocalRepository.Repositories;
-using Cts.TestData;
-using Cts.TestData.Identity;
 
 namespace LocalRepositoryTests;
 
 public static class RepositoryHelper
 {
-    public static LocalConcernRepository GetConcernRepository()
-    {
-        ClearAllStaticData();
-        return new LocalConcernRepository();
-    }
-
-    public static LocalOfficeRepository GetOfficeRepository()
-    {
-        ClearAllStaticData();
-        return new LocalOfficeRepository();
-    }
-
-    public static LocalUserStore GetLocalUserStore()
-    {
-        ClearAllStaticData();
-        return new LocalUserStore();
-    }
-
-    private static void ClearAllStaticData()
-    {
-        ConcernData.ClearData();
-        OfficeData.ClearData();
-        UserData.ClearData();
-    }
+    public static LocalOfficeRepository GetOfficeRepository() => new();
+    public static LocalUserStore GetLocalUserStore() => new();
 }

commit 8b5026298850fc21bfe07f205e2f1a0522fac046

Fix database seeding
SQL Server database seeding was broken by #912

src/EfRepository/Contexts/SeedDevData/DbSeedDataHelpers.cs
@@ -39,13 +39,19 @@ public static class DbSeedDataHelpers
         if (context.Complaints.Any()) return;
 
         if (context.Database.ProviderName == AppDbContext.SqlServerProvider)
+        {
+            context.Database.BeginTransaction();
             context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Complaints ON");
+        }
 
         context.Complaints.AddRange(ComplaintData.GetComplaints);
         context.SaveChanges();
 
         if (context.Database.ProviderName == AppDbContext.SqlServerProvider)
+        {
             context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Complaints OFF");
+            context.Database.CommitTransaction();
+        }
 
         if (!context.Attachments.Any())
             context.Attachments.AddRange(AttachmentData.GetAttachments);

tests/EfRepositoryTests/RepositoryHelper.cs
@@ -34,13 +34,11 @@ public sealed class RepositoryHelper : IDisposable, IAsyncDisposable
     /// </summary>
     private RepositoryHelper()
     {
-        _options = SqliteInMemory.CreateOptions<AppDbContext>(builder =>
-        {
-            builder.LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]);
-            builder.UseSeeding((context, _) => DbSeedDataHelpers.SeedAllData(context));
-        });
+        _options = SqliteInMemory.CreateOptions<AppDbContext>(builder: builder =>
+            builder.LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]));
         _context = new AppDbContext(_options);
         _context.Database.EnsureClean();
+        DbSeedDataHelpers.SeedAllData(_context);
     }
 
     /// <summary>
@@ -50,14 +48,12 @@ public sealed class RepositoryHelper : IDisposable, IAsyncDisposable
     /// <param name="callingMember">The unit test method requesting the Repository Helper.</param>
     private RepositoryHelper(object callingClass, string callingMember)
     {
-        _options = callingClass.CreateUniqueMethodOptions<AppDbContext>(callingMember: callingMember,
-            builder: builder =>
-            {
-                builder.UseSqlServer().LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]);
-                builder.UseSeeding((context, _) => DbSeedDataHelpers.SeedAllData(context));
-            });
+        _options = callingClass.CreateUniqueMethodOptions<AppDbContext>(builder: builder =>
+                builder.UseSqlServer().LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]),
+            callingMember);
         _context = new AppDbContext(_options);
         _context.Database.EnsureClean();
+        DbSeedDataHelpers.SeedAllData(_context);
     }
 
     /// <summary>