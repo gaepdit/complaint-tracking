9362b7425b7438bc780ddb625151fc47627d6b01
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-07-31 09:52:26 -0400

    Simplify and fix several unit tests

--- tests/EfRepositoryTests/Complaints/PublicSearchSpec.cs
+++ tests/EfRepositoryTests/Complaints/PublicSearchSpec.cs
@@ -48,6 +48,7 @@ public class PublicSearchSpec
     [Test]
     public async Task DateSpec_ReturnsFilteredList()
     {
+        // Arrange
         var spec = new ComplaintPublicSearchDto
         {
             DateFrom = DateOnly.FromDateTime(_referenceItem.ReceivedDate.Date),
@@ -55,83 +56,72 @@ public class PublicSearchSpec
         };
         var predicate = ComplaintFilters.PublicSearchPredicate(spec);
 
+        var expected = ComplaintData.GetComplaints
+            .Where(e => e.ReceivedDate == _referenceItem.ReceivedDate && !e.IsDeleted).ToList();
+
+        // Act
         var results = await _repository.GetListAsync(predicate);
 
-        var expected = ComplaintData.GetComplaints
-            .Where(e => e.ReceivedDate == _referenceItem.ReceivedDate && !e.IsDeleted);
-        results.Should().BeEquivalentTo(expected, opts => opts
-            .Excluding(e => e.CurrentOffice.StaffMembers)
-            .Excluding(e => e.CurrentOffice.Assignor)
-            .Excluding(e => e.Actions)
-            .Excluding(e => e.ComplaintTransitions)
-            .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy!.Office)
-            .Excluding(e => e.CurrentOwner!.Office)
-        );
+        // Assert
+        results.Should().HaveSameCount(expected);
+        results.Select(e => new { e.Id, e.ReceivedDate })
+            .Should().BeEquivalentTo(expected, options => options.ExcludingMissingMembers());
     }
 
     [Test]
     public async Task NatureSpec_ReturnsFilteredList()
     {
+        // Arrange
         var spec = new ComplaintPublicSearchDto { Description = _referenceItem.ComplaintNature };
         var predicate = ComplaintFilters.PublicSearchPredicate(spec);
+        var expected = ComplaintData.GetComplaints
+            .Where(e => e.ComplaintNature == _referenceItem.ComplaintNature && !e.IsDeleted).ToList();
 
+        // Act
         var results = await _repository.GetListAsync(predicate);
 
-        var expected = ComplaintData.GetComplaints
-            .Where(e => e.ComplaintNature == _referenceItem.ComplaintNature && !e.IsDeleted);
-        results.Should().BeEquivalentTo(expected, opts => opts
-            .Excluding(e => e.CurrentOffice.StaffMembers)
-            .Excluding(e => e.CurrentOffice.Assignor)
-            .Excluding(e => e.Actions)
-            .Excluding(e => e.ComplaintTransitions)
-            .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy!.Office)
-            .Excluding(e => e.CurrentOwner!.Office)
-        );
+        // Assert
+        results.Should().HaveSameCount(expected);
+        results.Select(e => new { e.Id, e.SourceAddress })
+            .Should().BeEquivalentTo(expected, options => options.ExcludingMissingMembers());
     }
 
     [Test]
     public async Task ConcernSpec_ReturnsFilteredList()
     {
+        // Arrange
         var spec = new ComplaintPublicSearchDto { Concern = _referenceItem.PrimaryConcern.Id };
         var predicate = ComplaintFilters.PublicSearchPredicate(spec);
 
+        var expected = ComplaintData.GetComplaints
+            .Where(e => e.PrimaryConcern.Id == _referenceItem.PrimaryConcern.Id && !e.IsDeleted).ToList();
+
+        // Act
         var results = await _repository.GetListAsync(predicate);
 
-        var expected = ComplaintData.GetComplaints
-            .Where(e => e.PrimaryConcern.Id == _referenceItem.PrimaryConcern.Id && !e.IsDeleted);
-        results.Should().BeEquivalentTo(expected, opts => opts
-            .Excluding(e => e.CurrentOffice.StaffMembers)
-            .Excluding(e => e.CurrentOffice.Assignor)
-            .Excluding(e => e.Actions)
-            .Excluding(e => e.ComplaintTransitions)
-            .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy!.Office)
-            .Excluding(e => e.CurrentOwner!.Office)
-            .Excluding(e => e.Attachments)
-        );
+        // Assert
+        results.Should().HaveSameCount(expected);
+        results.Select(e => new { e.Id, e.PrimaryConcern })
+            .Should().BeEquivalentTo(expected, options => options.ExcludingMissingMembers());
     }
 
     [Test]
     public async Task SourceNameSpec_ReturnsFilteredList()
     {
+        // Arrange
         var spec = new ComplaintPublicSearchDto { SourceName = _referenceItem.SourceFacilityName };
         var predicate = ComplaintFilters.PublicSearchPredicate(spec);
 
+        var expected = ComplaintData.GetComplaints
+            .Where(e => e.SourceFacilityName == _referenceItem.SourceFacilityName && !e.IsDeleted).ToList();
+
+        // Act
         var results = await _repository.GetListAsync(predicate);
 
-        var expected = ComplaintData.GetComplaints
-            .Where(e => e.SourceFacilityName == _referenceItem.SourceFacilityName && !e.IsDeleted);
-        results.Should().BeEquivalentTo(expected, opts => opts
-            .Excluding(e => e.CurrentOffice.StaffMembers)
-            .Excluding(e => e.CurrentOffice.Assignor)
-            .Excluding(e => e.Actions)
-            .Excluding(e => e.ComplaintTransitions)
-            .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy!.Office)
-            .Excluding(e => e.CurrentOwner!.Office)
-        );
+        // Arrange
+        results.Should().HaveSameCount(expected);
+        results.Select(e => new { e.Id, e.SourceFacilityName })
+            .Should().BeEquivalentTo(expected, options => options.ExcludingMissingMembers());
     }
 
     [Test]
@@ -160,117 +150,107 @@ public class PublicSearchSpec
     [Test]
     public async Task StreetSpec_ReturnsFilteredList()
     {
+        // Arrange
         var spec = new ComplaintPublicSearchDto { Street = _referenceItem.SourceAddress!.Street };
         var predicate = ComplaintFilters.PublicSearchPredicate(spec);
-
-        var results = await _repository.GetListAsync(predicate);
 
         var expected = ComplaintData.GetComplaints
             .Where(e => e.SourceAddress != null
                         && e.SourceAddress.Street == _referenceItem.SourceAddress.Street
-                        && !e.IsDeleted);
-        results.Should().BeEquivalentTo(expected, opts => opts
-            .Excluding(e => e.CurrentOffice.StaffMembers)
-            .Excluding(e => e.CurrentOffice.Assignor)
-            .Excluding(e => e.Actions)
-            .Excluding(e => e.ComplaintTransitions)
-            .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy!.Office)
-            .Excluding(e => e.CurrentOwner!.Office)
-        );
+                        && !e.IsDeleted).ToList();
+
+        // Act
+        var results = await _repository.GetListAsync(predicate);
+
+        // Assert
+        results.Should().HaveSameCount(expected);
+        results.Select(e => new { e.Id, e.SourceAddress })
+            .Should().BeEquivalentTo(expected, options => options.ExcludingMissingMembers());
     }
 
     [Test]
     public async Task Street2Spec_ReturnsFilteredList()
     {
+        // Act
+
         // "Street" spec filter matches either Street OR Street2 from address
         var spec = new ComplaintPublicSearchDto { Street = _referenceItem.SourceAddress!.Street2 };
         var predicate = ComplaintFilters.PublicSearchPredicate(spec);
 
-        var results = await _repository.GetListAsync(predicate);
-
         var expected = ComplaintData.GetComplaints
             .Where(e => e.SourceAddress != null
                         && e.SourceAddress.Street2 == _referenceItem.SourceAddress.Street2
-                        && !e.IsDeleted);
-        results.Should().BeEquivalentTo(expected, opts => opts
-            .Excluding(e => e.CurrentOffice.StaffMembers)
-            .Excluding(e => e.CurrentOffice.Assignor)
-            .Excluding(e => e.Actions)
-            .Excluding(e => e.ComplaintTransitions)
-            .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy!.Office)
-            .Excluding(e => e.CurrentOwner!.Office)
-        );
+                        && !e.IsDeleted).ToList();
+
+        // Act
+        var results = await _repository.GetListAsync(predicate);
+
+        // Assert
+        results.Should().HaveSameCount(expected);
+        results.Select(e => new { e.Id, e.SourceAddress })
+            .Should().BeEquivalentTo(expected, options => options.ExcludingMissingMembers());
     }
 
     [Test]
     public async Task CitySpec_ReturnsFilteredList()
     {
+        // Arrange
         var spec = new ComplaintPublicSearchDto { City = _referenceItem.SourceAddress!.City };
         var predicate = ComplaintFilters.PublicSearchPredicate(spec);
-
-        var results = await _repository.GetListAsync(predicate);
-
         var expected = ComplaintData.GetComplaints
             .Where(e => e.SourceAddress != null
                         && e.SourceAddress.City == _referenceItem.SourceAddress.City
-                        && !e.IsDeleted);
-        results.Should().BeEquivalentTo(expected, opts => opts
-            .Excluding(e => e.CurrentOffice.StaffMembers)
-            .Excluding(e => e.CurrentOffice.Assignor)
-            .Excluding(e => e.Actions)
-            .Excluding(e => e.ComplaintTransitions)
-            .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy!.Office)
-            .Excluding(e => e.CurrentOwner!.Office)
-        );
+                        && !e.IsDeleted).ToList();
+
+        // Act
+        var results = await _repository.GetListAsync(predicate);
+
+        // Assert
+        results.Should().HaveSameCount(expected);
+        results.Select(e => new { e.Id, e.SourceAddress })
+            .Should().BeEquivalentTo(expected, options => options.ExcludingMissingMembers());
     }
 
     [Test]
     public async Task StateSpec_ReturnsFilteredList()
     {
+        // Arrange
         var spec = new ComplaintPublicSearchDto { State = _referenceItem.SourceAddress!.State };
         var predicate = ComplaintFilters.PublicSearchPredicate(spec);
-
-        var results = await _repository.GetListAsync(predicate);
 
         var expected = ComplaintData.GetComplaints
             .Where(e => e.SourceAddress != null
                         && e.SourceAddress.State == _referenceItem.SourceAddress.State
-                        && !e.IsDeleted);
-        results.Should().BeEquivalentTo(expected, opts => opts
-            .Excluding(e => e.CurrentOffice.StaffMembers)
-            .Excluding(e => e.CurrentOffice.Assignor)
-            .Excluding(e => e.Actions)
-            .Excluding(e => e.ComplaintTransitions)
-            .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy!.Office)
-            .Excluding(e => e.CurrentOwner!.Office)
-        );
+                        && !e.IsDeleted).ToList();
+
+        // Act
+        var results = await _repository.GetListAsync(predicate);
+
+        // Assert
+        results.Should().HaveSameCount(expected);
+        results.Select(e => new { e.Id, e.SourceAddress })
+            .Should().BeEquivalentTo(expected, options => options.ExcludingMissingMembers());
     }
 
     [Test]
     public async Task PostalCodeSpec_ReturnsFilteredList()
     {
+        // Arrange
         var spec = new ComplaintPublicSearchDto { PostalCode = _referenceItem.SourceAddress!.PostalCode };
         var predicate = ComplaintFilters.PublicSearchPredicate(spec);
-
         await using var repository = RepositoryHelper.CreateRepositoryHelper().GetComplaintRepository();
-        var results = await repository.GetListAsync(predicate);
 
         var expected = ComplaintData.GetComplaints
             .Where(e => e.SourceAddress != null
                         && e.SourceAddress.PostalCode == _referenceItem.SourceAddress.PostalCode
-                        && !e.IsDeleted);
-        results.Should().BeEquivalentTo(expected, opts => opts
-            .Excluding(e => e.CurrentOffice.StaffMembers)
-            .Excluding(e => e.CurrentOffice.Assignor)
-            .Excluding(e => e.Actions)
-            .Excluding(e => e.ComplaintTransitions)
-            .Excluding(e => e.EnteredBy!.Office)
-            .Excluding(e => e.ReceivedBy!.Office)
-            .Excluding(e => e.CurrentOwner!.Office)
-        );
+                        && !e.IsDeleted).ToList();
+
+        // Act
+        var results = await repository.GetListAsync(predicate);
+
+        // Act
+        results.Should().HaveSameCount(expected);
+        results.Select(e => new { e.Id, e.SourceAddress })
+            .Should().BeEquivalentTo(expected, options => options.ExcludingMissingMembers());
     }
 }
