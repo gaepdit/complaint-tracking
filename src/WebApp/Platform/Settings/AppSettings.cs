2167754eddaa5f347b035e8f5b961a7bca560f7e
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2026-03-03 14:11:13 -0500

    Add DataDog front-end monitoring (#950)

--- src/WebApp/Platform/Settings/AppSettings.cs
+++ src/WebApp/Platform/Settings/AppSettings.cs
@@ -8,14 +8,26 @@ internal static partial class AppSettings
 {
     // Support settings
     public static string? Version { get; private set; }
+    public static string? SimpleVersion => Version?.Split('+')[0];
+    
+    public static string Env { get; } = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "unknown";
+    public static string ShortEnv => Env switch { "Production" => "prod", "Staging" => "uat", _ => "dev" };
+
     public static Support Support { get; } = new();
     public static EntraIdPhaseOut EntraIdPhaseOut { get; } = new();
     public static Raygun RaygunSettings { get; } = new();
+    public static DataDog DataDogSettings { get; } = new();
     public static string? OrgNotificationsApiUrl { get; private set; }
 
     public record Raygun
     {
         public string? ApiKey { get; [UsedImplicitly] init; }
+    }
+
+    public record DataDog
+    {
+        public string? ClientToken { get; [UsedImplicitly] init; }
+        public string? ApplicationId { get; [UsedImplicitly] init; }
     }
 
     // Attachment File Service configuration
