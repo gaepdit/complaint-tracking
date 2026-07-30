b1abbb8f8de896d9e9d0dc73eb12e6997ee6be5a
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-07-30 11:16:16 -0400

    Fix CSP for Datadog

--- src/WebApp/Platform/AppConfiguration/SecurityHeaders.cs
+++ src/WebApp/Platform/AppConfiguration/SecurityHeaders.cs
@@ -65,7 +65,7 @@ internal static class SecurityHeaders
         builder.AddBaseUri().None();
         builder.AddObjectSrc().None();
         builder.AddScriptSrc().Self()
-            .From("https://www.datadoghq-browser-agent.com/us3/v6/")
+            .From("https://www.datadoghq-browser-agent.com/us3/")
             .WithHashTagHelper()
             .WithNonce()
             .ReportSample();
@@ -84,7 +84,7 @@ internal static class SecurityHeaders
             .From("https://uat-aux.gaepd.org")
             .From("https://dev-aux.gaepd.org");
         builder.AddWorkerSrc()
-            .From("https://www.datadoghq-browser-agent.com/us3/v6/");
+            .From("https://www.datadoghq-browser-agent.com/us3/");
         builder.AddReportTo("csp-endpoint");
     }
 #pragma warning restore S1075
